375c90173d585132e613fe54c2ddb1e85942827f
Stephen <srr@routhouse.org>
2024-10-02 09:30:39 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	if (nums.length == 0) {
+    return 0;
+  }
+  
+  return nums.reduce((total, cur) => {
+    return total + cur;
+  })
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  if (nums.length == 0) {
+    return 0;
+  }
 
+  return nums.reduce((total, cur) => {
+    return total * cur;
+  })
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  while (a > 0) {
+    total = total * a;
+    a--;
+  }
+  return total;
 };
 
 // Do not edit below this line
