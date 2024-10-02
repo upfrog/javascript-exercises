ea1f56b19c988cc57849f313e582320a207fcfc5
Stephen <srr@routhouse.org>
2024-10-02 11:27:15 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    oldest = people[0];
+    oldestAge = calcAge(people[0]);
+
+    for (let i = 1; i < people.length; i++) {
+        
+        personAge = calcAge(people[i]);
+
+        if (personAge > oldestAge) {
+            oldest = people[i];
+            oldestAge = personAge;
+        }
+    }
+
+    return oldest;
+    
 };
+
+
+function calcAge(person) {
+    let yod = person.yearOfDeath;
+
+    if (yod == undefined) {
+        yod = new Date().getFullYear();
+    }
+
+    return yod - person.yearOfBirth;
+}
+
+main();
+function main() {
+    findTheOldest("F");
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
