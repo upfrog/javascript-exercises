672b8ca8fd475ec161ad13ea439c9934f3c08c67
Stephen <srr@routhouse.org>
2024-10-02 10:25:10 -0400

    Complete exercise 11 - though it wasn't much

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
 
+    let titleArray = bookArray.map((book) => {
+        return book.title;
+    })
+
+    return titleArray;
 };
 
 // Do not edit below this line
